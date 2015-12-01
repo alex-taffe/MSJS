44c2f390d3ea1ecb2ee43f7fbc10445f6af3a1f5
Alex Taffe <alex.taffe@gmail.com>
2015-12-01 07:49:09 -0500

    Add keyboard controls to login form

--- Server/sodium-pathway-93914/js/home.js
+++ Server/sodium-pathway-93914/js/home.js
@@ -1,89 +1,96 @@
-var debug = true;
-$(document).ready(function () {
-    if (debug) {
-        $("#panelNav").load("panel-nav.html", function () {});
-        $(".container").load("panel.html", null);
-        $("#demo-canvas").css('filter', filterVal).css('-webkit-filter', filterVal).css('-moz-filter', filterVal).css('-o-filter', filterVal).css('-ms-filter', filterVal);
+var debug = false;
+
+
+
+function checkNext(e) {
+    if (e.keyCode == 13) {
+        $("#password").focus();
     }
+}
 
-
-    function login() {
-        $("#loginAlert").html('');
-        $("#loginForm").css("visibility", "hidden");
-        $("#loginLoader").css("visibility", "visible");
-        $.post("login", {
-                email: $("#email").val(),
-                password: $("#password").val()
-            })
-            .done(function (data) {
-                var loginInfo = jQuery.parseJSON(data);
-                if (loginInfo.errorCode == 200) {
-                    $('#loginModal').modal('hide');
-                    $('#loginModal').on('hidden.bs.modal', function (e) {
-                        var filterVal = 'blur(5px)';
-                        $("#demo-canvas").css('filter', filterVal).css('-webkit-filter', filterVal).css('-moz-filter', filterVal).css('-o-filter', filterVal).css('-ms-filter', filterVal);
-                        $(".container").load("panel.html", function () {
-                            getLessons();
-                            $("#addLesson").click(function () {
-                                $.post("add-lesson", {
-                                        JSON: $("#enterJSON").val()
-                                    })
-                                    .done(function (data) {
-                                        $('#addLessonModal').modal('hide');
-                                        $('#addLessonModal').on('hidden.bs.modal', function (e) {
-                                            $("#enterJSON").val("");
-                                        });
-                                        getLessons();
-                                    });
-                            });
-                        });
-                        $("#panelNav").load("panel-nav.html", function () {});
-                    });
-                } else {
-                    $("#loginAlert").html('<div class="alert in alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + loginInfo.message + '</div>');
-                    $("#password").val("");
-                    $("#loginForm").css("visibility", "visible");
-                    $("#loginLoader").css("visibility", "hidden");
-                }
-            });
-    };
-
-    function register() {
-        $("#registerAlert").html('');
-        $("#registerForm").css("visibility", "hidden");
-        $("#registerLoader").css("visibility", "visible");
-        var recaptcha = null;
-        if (typeof grecaptcha !== 'undefined')
-            recaptcha = grecaptcha.getResponse();
-        $.post("register", {
-                email: $("#email").val(),
-                password: $("#password").val(),
-                password2: $("#password2").val(),
-                "g-recaptcha-response": recaptcha
-            })
-            .done(function (data) {
-                var registerInfo = jQuery.parseJSON(data);
-                if (registerInfo.errorCode == 200) {
-                    $('#registerModal').modal('hide');
-                    $('#registerModal').on('hidden.bs.modal', function (e) {
-                        login();
-                    });
-                } else {
-                    $("#registerAlert").html('<div class="alert in alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + registerInfo.message + '</div>');
-                    $("#password").val("");
-                    $("#password2").val("");
-                    $("#registerForm").css("visibility", "visible");
-                    $("#registerLoader").css("visibility", "hidden");
-                }
-            });
-    }
-    $(".loginLogin").click(function () {
+function checkLogin(e) {
+    if (e.keyCode == 13) {
         login();
-    });
-    $("#registerFinal").click(function () {
-        register();
-    });
+    }
+}
+
+function login() {
+    $("#loginAlert").html('');
+    $("#loginForm").css("visibility", "hidden");
+    $("#loginLoader").css("visibility", "visible");
+    $.post("login", {
+            email: $("#email").val(),
+            password: $("#password").val()
+        })
+        .done(function (data) {
+            var loginInfo = jQuery.parseJSON(data);
+            if (loginInfo.errorCode == 200) {
+                $('#loginModal').modal('hide');
+                $('#loginModal').on('hidden.bs.modal', function (e) {
+                    var filterVal = 'blur(5px)';
+                    $("#demo-canvas").css('filter', filterVal).css('-webkit-filter', filterVal).css('-moz-filter', filterVal).css('-o-filter', filterVal).css('-ms-filter', filterVal);
+                    $(".container").load("panel.html", function () {
+                        getLessons();
+                        $("#addLesson").click(function () {
+                            $.post("add-lesson", {
+                                    JSON: $("#enterJSON").val()
+                                })
+                                .done(function (data) {
+                                    $('#addLessonModal').modal('hide');
+                                    $('#addLessonModal').on('hidden.bs.modal', function (e) {
+                                        $("#enterJSON").val("");
+                                    });
+                                    getLessons();
+                                });
+                        });
+                    });
+                    $("#panelNav").load("panel-nav.html", function () {});
+                });
+            } else {
+                $("#loginAlert").html('<div class="alert in alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + loginInfo.message + '</div>');
+                $("#password").val("");
+                $("#loginForm").css("visibility", "visible");
+                $("#loginLoader").css("visibility", "hidden");
+            }
+        });
+};
+
+function register() {
+    $("#registerAlert").html('');
+    $("#registerForm").css("visibility", "hidden");
+    $("#registerLoader").css("visibility", "visible");
+    var recaptcha = null;
+    if (typeof grecaptcha !== 'undefined')
+        recaptcha = grecaptcha.getResponse();
+    $.post("register", {
+            email: $("#email").val(),
+            password: $("#password").val(),
+            password2: $("#password2").val(),
+            "g-recaptcha-response": recaptcha
+        })
+        .done(function (data) {
+            var registerInfo = jQuery.parseJSON(data);
+            if (registerInfo.errorCode == 200) {
+                $('#registerModal').modal('hide');
+                $('#registerModal').on('hidden.bs.modal', function (e) {
+                    login();
+                });
+            } else {
+                $("#registerAlert").html('<div class="alert in alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>' + registerInfo.message + '</div>');
+                $("#password").val("");
+                $("#password2").val("");
+                $("#registerForm").css("visibility", "visible");
+                $("#registerLoader").css("visibility", "hidden");
+            }
+        });
+}
+$(".loginLogin").click(function () {
+    login();
 });
+$("#registerFinal").click(function () {
+    register();
+});
+
 
 function getLessons() {
     $.getJSON("lessons", function (data) {
@@ -118,3 +125,11 @@ function getLessons() {
         }
     });
 }
+
+$(document).ready(function () {
+    if (debug) {
+        $("#panelNav").load("panel-nav.html", function () {});
+        $(".container").load("panel.html", null);
+        $("#demo-canvas").css('filter', filterVal).css('-webkit-filter', filterVal).css('-moz-filter', filterVal).css('-o-filter', filterVal).css('-ms-filter', filterVal);
+    }
+});
